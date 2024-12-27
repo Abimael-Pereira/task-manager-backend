393ba769bf660ca1fb158de42d575f65ab6eabcf
Abimael <abimael.spdev@gmail.com>
2024-12-26 23:27:10 -0400

    feat: move task endpoints to a controller

--- src/routes/task.routes.js
+++ src/routes/task.routes.js
@@ -6,74 +6,23 @@ const TaskModel = require("../models/task.model");
 const router = express.Router();
 
 router.get("/", async (req, res) => {
-    return new TaskController(req, res).getTasks();
+    return new TaskController(req, res).getAll();
 });
 
 router.get("/:id", async (req, res) => {
-    try {
-        const taskId = req.params.id;
-        const task = await TaskModel.findById(taskId);
-
-        if (!task) {
-            res.status(404).send("Essa tarefa não foi encontrada");
-        }
-        return res.status(200).send(task);
-    } catch (error) {
-        res.status(500).send(error.message);
-    }
+    return new TaskController(req, res).getById();
 });
 
 router.post("/", async (req, res) => {
-    try {
-        const newTask = new TaskModel(req.body);
-        await newTask.save();
-        res.status(201).send(newTask);
-    } catch (error) {
-        res.status(500).send(error.message);
-    }
+    return new TaskController(req, res).create();
 });
 
 router.patch("/:id", async (req, res) => {
-    try {
-        const taskId = req.params.id;
-        const taskData = req.body;
-
-        const taskToUpdate = await TaskModel.findById(taskId);
-
-        const allowedUpdates = ["isCompleted"];
-        const requestedUpdates = Object.keys(taskData);
-
-        for (update of requestedUpdates) {
-            if (allowedUpdates.includes(update)) {
-                taskToUpdate[update] = taskData[update];
-            } else {
-                return res
-                    .status(500)
-                    .send("Um ou mais campos não são editáveis");
-            }
-        }
-
-        await taskToUpdate.save();
-        return res.status(200).send("Updated!");
-    } catch (error) {
-        return res.status(500).send(error.message);
-    }
+    return new TaskController(req, res).update();
 });
 
 router.delete("/:id", async (req, res) => {
-    try {
-        const taskId = req.params.id;
-        const taskToDelete = await TaskModel.findById(taskId);
-
-        if (!taskToDelete) {
-            res.status(404).send("Essa tarefa não foi encontrada");
-        }
-
-        const deletedTask = await TaskModel.findByIdAndDelete(taskId);
-        res.status(200).send(deletedTask);
-    } catch (error) {
-        res.status(500).send(error.message);
-    }
+    return new TaskController(req, res).delete();
 });
 
 module.exports = router;
